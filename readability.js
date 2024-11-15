6ad059d45bfe3e4c6842b1a5dabf9930a5737f65
Julian Hasreiter <75797743+Pinguin2001@users.noreply.github.com>
2024-11-15 22:19:08 +0100

    feat: Dark mode support

--- readability.js
+++ readability.js
@@ -4,7 +4,7 @@ const article = new Readability(document).parse();
 var title = document.getElementsByTagName("title")[0].innerHTML;
 
 const titleHeader = "<title>" + title + "</title>\n";
-const stylesHeader = "<style>body {font-family:'Segoe UI'; margin:auto; margin-top:50px; width:60%;}</style>"
+const stylesHeader = "<style>body {font-family:'Segoe UI'; margin:auto; margin-top:50px; width:60%;} @media (prefers-color-scheme: dark) { body { background-color: #202020; color: white; } a:link { color: #9cdcfe; } a:visited { color: #9cdcfe; }}</style>"
 
 document.getElementsByTagName("head")[0].innerHTML = titleHeader + stylesHeader;
 document.body.innerHTML = null;
