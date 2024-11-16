b673a742e4b904bbb387709940eff9fc4dff07e6
Julian Hasreiter <75797743+Pinguin2001@users.noreply.github.com>
2024-11-16 15:45:40 +0100

    fix: Use readability provided article title

--- readability.js
+++ readability.js
@@ -1,7 +1,8 @@
 import Readability from "./libs/readability.js";
 
 const article = new Readability(document).parse();
-var title = document.getElementsByTagName("title")[0].innerHTML;
+const title = article.title;
+const content = article.content;
 
 const titleHeader = "<title>" + title + "</title>\n";
 const stylesHeader = "<style>body { font-family: 'Segoe UI'; max-width: 600px; margin: auto; margin-top: 20px; } img { max-width: 100%; height: auto; } @media (prefers-color-scheme: dark) { body { background-color: #202020; color: white; } a:link { color: #9cdcfe; } a:visited { color: #9cdcfe; } }</style>"
@@ -9,4 +10,4 @@ const stylesHeader = "<style>body { font-family: 'Segoe UI'; max-width: 600px; m
 document.getElementsByTagName("head")[0].innerHTML = titleHeader + stylesHeader;
 document.body.innerHTML = null;
 document.body.innerHTML = "<h2>" + title + "</h2>";
-document.body.innerHTML += article.content;
+document.body.innerHTML += content;
